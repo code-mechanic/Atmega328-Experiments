7f318bccba21d3b57a6e5f74d93b0e505170b00c
code-mechanic <hashincludefun.h@gmail.com>
2019-12-05 09:31:47 +0530

    SIMULTANEOUS SQUARE WAVE + ONE PRESS LED ON/OFF

--- Switch-and-LED/switch-Led.c
+++ Switch-and-LED/switch-Led.c
@@ -3,28 +3,37 @@
 #endif
 #include <avr/io.h> 
 #include <util/delay.h> 
-
-int change = 1;
+char change = 1;
+char release_check;
 int main(void)
 {
   DDRB = 0x30; // PB5 as OUTPUT
   DDRB = DDRB & 0xFE; // PB0 as INPUT
   while (1)
   {
+    PORTB |= 0x10; 
+    _delay_ms(100);
+    PORTB &= 0xEF; 
+    _delay_ms(100);
     if ((PINB & 0x01) == 1) 
     {
-      if(change == 1)
+      if(change == 0xFE) // old 1
       {
-        PORTB = 0x20;
-        change = 0;
-        while((PINB & 0x01) == 1);
+        PORTB |= 0x20;
       }
-      else if(change == 0)
+      else if(change == 1) // old 0
       {
-        PORTB = 0x00;
-        change = 1;
-        while((PINB & 0x01) == 1);
+        PORTB &= 0xDF;
       }
-    } 
+      release_check = 1;
+    }
+    if ((PINB & 0x01) == 0)
+    {
+      if(release_check == 1)
+      {
+        change = ~change;
+        release_check = 0;
+      }
+    }
   }
 } 
